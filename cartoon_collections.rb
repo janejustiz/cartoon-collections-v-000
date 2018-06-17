951a097e291612ef41d209c600cde3d405444216
Jane Justiz <janejustiz@gmail.com>
2018-06-17 01:13:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0
     while i < array.length
-      if array[i] == cheese_types.each
+      if array[i].include?(cheese_types[i])
         return array[i]
       end
       i = i + 1
